8ad65fb5c72d5243fa585b8975513353ec2c2ede
u-kai <76635578+u-kai@users.noreply.github.com>
2024-09-16 09:09:47 +0900

    chore version up func

--- usecase.go
+++ usecase.go
@@ -5,25 +5,34 @@ type TagList interface {
 }
 
 func MinorUpAll(tagList TagList) (*[]*ServiceTagWithSemVer, error) {
-	tags, err := tagList.List()
-	if err != nil {
-		return nil, err
-	}
-
-	serviceTags := []*ServiceTagWithSemVer{}
-	if tags == nil || len(*tags) == 0 {
-		return &serviceTags, nil
-	}
-
-	for _, tag := range *tags {
-		serviceTag, err := tag.ToServiceTag()
-		if err != nil {
-			continue
-		}
-		serviceTag.UpdateMinor()
-		serviceTags = append(serviceTags, serviceTag)
-	}
-
-	return &serviceTags, nil
+	return versionUpAll(func(tag *ServiceTagWithSemVer) {
+		tag.UpdateMinor()
+	})(tagList)
 }
 
+type versionUpFunc func(*ServiceTagWithSemVer)
+
+func versionUpAll(f versionUpFunc) func(tagList TagList) (*[]*ServiceTagWithSemVer, error) {
+	return func(tagList TagList) (*[]*ServiceTagWithSemVer, error) {
+		tags, err := tagList.List()
+		if err != nil {
+			return nil, err
+		}
+
+		serviceTags := []*ServiceTagWithSemVer{}
+		if tags == nil || len(*tags) == 0 {
+			return &serviceTags, nil
+		}
+
+		for _, tag := range *tags {
+			serviceTag, err := tag.ToServiceTag()
+			if err != nil {
+				continue
+			}
+			f(serviceTag)
+			serviceTags = append(serviceTags, serviceTag)
+		}
+
+		return &serviceTags, nil
+	}
+}
